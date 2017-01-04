bf8c0f416951795300addfe613c87cc1a6332686
loserStar <362527240@qq.com>
2017-01-04 12:01:08 +0800

    文档

---
+++ doc/数据库设计/创建语句.sql
@@ -0,0 +1,17 @@
+﻿--建表空间
+CREATE TABLESPACE mcyl_data DATAFILE 'D:\ORACL\mcyl.dbf' SIZE  256M AUTOEXTEND ON NEXT 32M MAXSIZE UNLIMITED LOGGING EXTENT MANAGEMENT LOCAL SEGMENT SPACE MANAGEMENT  AUTO
+
+
+
+-- Create the user 
+create user mcyl
+  default tablespace mcyl_data identified by mcyl
+  temporary tablespace TEMP
+  profile DEFAULT;
+-- Grant/Revoke role privileges 
+grant resource to mcyl;
+-- Grant/Revoke system privileges 
+grant create session to mcyl;
+grant unlimited tablespace to mcyl;
+--给用户DBA权限
+grant DBA to mcyl
